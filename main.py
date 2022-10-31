0e32c8911228dfc46b091e8bcab88ef890d4b6ed
scottshanfeld <74272645+scottshanfeld@users.noreply.github.com>
2022-10-31 11:23:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,3 @@ app = create_app()
 if __name__ == '__main__':
     app.run(debug=True)
 
-# @app.route("/psu") #deterrmines url extension
-# def index():
-#     return render_template("index.html")
-
-# @app.route("/process", methods ["POST", "GET"]) #deterrmines url extension
-# def submit():
-#     transcript = request.form["transcript_input"] #may not work for saving file
-#     return render_template("index.html")
